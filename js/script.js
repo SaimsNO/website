fdfcadacd55a405fa691a12b0b8e8cd2a8bf370d
Saims_no <blitztownone@gmail.com>
2023-10-06 13:59:36 +0300

    добавить картинки для товаров, изменить стили

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
-    nav:false,
+    
     responsive:{
         0:{
-            items:1
+            items:1,
+            nav:false,
         },
         600:{
-            items:1
+            items:1,
+            nav:false,
         },
         1000:{
-            items:1
+            items:1,
+            nav:true,
         }
     }
 })
